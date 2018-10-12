3bd4adc627bf14ff881b2bb8e2157abfca1c8f12
Anmol-Singh <anmoldhillon999@gmail.com>
2018-10-12 10:13:23 -0500

    first commit

---
+++ Assignment1ForBugTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment1ForBugTracker.Startup))]
+namespace Assignment1ForBugTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
